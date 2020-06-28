8caa90e4ef7e514135c8637f7ebc3f870faa0744
MrReese0 <rsengup7@asu.edu>
2020-06-28 05:12:25 -0700

    Updates

---
+++ precache-manifest.ed6a91c7a38ec715cdbee24a0f60dce6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab01855ae9b17e105b4fcbac0f12e78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5e57a822ef9772fa76d",
+    "url": "/static/css/main.5ec70088.chunk.css"
+  },
+  {
+    "revision": "7acf6dc384a9e7fe9da9",
+    "url": "/static/js/2.189ccc48.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.189ccc48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e57a822ef9772fa76d",
+    "url": "/static/js/main.6ab2be03.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "f11224c2a549e6bdf25e23d38933368b",
+    "url": "/static/media/BGtest4.f11224c2.png"
+  },
+  {
+    "revision": "a927a43262b6460747a314ca4bcbad09",
+    "url": "/static/media/background.a927a432.jpg"
+  },
+  {
+    "revision": "ae67f407fdbd89b4ec8ced494259f489",
+    "url": "/static/media/testBW2.ae67f407.png"
+  }
+]);
